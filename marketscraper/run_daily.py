9ea1664ea4a60ff6e831211dd226d63de4dbb154
nzervos98 <58821751+nzervos98@users.noreply.github.com>
2025-09-19 16:08:14 +0300

    Add files via upload

--- marketscraper/run_daily.py
+++ marketscraper/run_daily.py
@@ -3,7 +3,7 @@ import datetime
 import os
 from datetime import date
 
-# Απόλυτο path του project (ο φάκελος που περιέχει το settings.py)
+# Απόλυτο path του project
 PROJECT_DIR = r"C:\Users\user\Documents\Python\PythonProject\marketscraper\marketscraper"
 
 # Path προς το python.exe του virtual environment
@@ -11,8 +11,8 @@ VENV_PYTHON = r"C:\Users\user\Documents\Python\PythonProject\.venv\Scripts\pytho
 
 
 def run_spider():
-    print(f"[{datetime.datetime.now()}] ▶️ Εκκίνηση spider...")
-    with open("copy.txt", "a", encoding="utf-8") as file:
+    print(f"[{datetime.datetime.now()}] ▶️ Εκκίνηση crawl...")
+    with open("marketscraper/copy.txt", "a", encoding="utf-8") as file:
         file.write(f'----------{date.today()}----------\n')
 
     subprocess.run(
@@ -20,7 +20,7 @@ def run_spider():
         cwd=PROJECT_DIR
     )
 
-    print(f"[{datetime.datetime.now()}] ✅ Ολοκληρώθηκε scraping.\n")
+    print(f"[{datetime.datetime.now()}] ✅ Ολοκληρώθηκε.\n")
 
 
 if __name__ == "__main__":
